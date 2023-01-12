cc45be8c8054ba238de342634748cb8a93cd1115
congminh98 <congminhphan98@gmail.com>
2023-01-12 15:29:59 +0700

    .

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cevog7sgqg4cnphki85g-a.oregon-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi_ljd3'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', 'xG49aHFQEKTqxxbNvQCMIiehoyGIuuDz'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
